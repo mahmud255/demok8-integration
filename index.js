d15a809396827c7a4d36ab79d32e99e262b0a269
Mahmudur Rahman <tareqmahmud4@gmail.com>
2022-09-07 18:16:10 +0600

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From Mahmud" }');
+    res.send('Thanks! It's working!!');
 });
 
 app.get('/will', function (req, res) {
-    res.send('{ "response": "Hello World" }');
+    res.send('Hello World');
 });
 app.get('/ready', function (req, res) {
-    res.send('{ "response": " Great!, It works!" }');
+    res.send('Great!, It works!');
 });
 app.listen(process.env.PORT || 3000);
 module.exports = app;
