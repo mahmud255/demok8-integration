eaa2b93176cb1b6dc67a4e53cc63d67021fac147
Mahmudur Rahman <tareqmahmud4@gmail.com>
2022-09-08 17:22:52 +0600

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Its working, Thanks!" }');
+    res.send('"Its working, Thanks!"');
 });
 
 app.get('/will', function (req, res) {
